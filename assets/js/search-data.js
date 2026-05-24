adfecaa20792e5bd768b7c58b9900031008280a3
SlavaChepelin <119756087+SlavaChepelin@users.noreply.github.com>
2026-05-24 16:15:25 +0000

    Deploying to gh-pages from @ VyacheslavChepelin/about-me@a6a175c0cd44ac8a11c126eb6422af31f31866d0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -92,12 +92,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/about-me/projects/1_project/";
-            },},{id: "projects-ct-notes-y2024",
+          section: "News",},{id: "projects-ct-notes-y2024",
           title: 'CT Notes y2024',
           description: "",
           section: "Projects",handler: () => {
