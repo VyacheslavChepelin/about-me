1a3ba836cc745fa59dfcf67bdf937935e6dfcb00
SlavaChepelin <119756087+SlavaChepelin@users.noreply.github.com>
2026-09-24 15:27:07 +0000

    Deploying to gh-pages from @ VyacheslavChepelin/about-me@b53faf909f067d3824e6198873fee7e0eaa9120e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -179,13 +179,6 @@ ninja.data = [{
           window.open("https://github.com/VyacheslavChepelin", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://vk.com/slavachepelin", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
